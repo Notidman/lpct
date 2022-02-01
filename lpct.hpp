b918c6715242faf395d68e61e58ced3be037451f
Notidman <laiker35@yandex.ru>
2022-02-01 06:44:19 +0400

    --added more features

--- lpct.hpp
+++ lpct.hpp
@@ -11,56 +11,69 @@
 namespace lpct {
 
   #include <iostream>
-  #ifdef _WIN32 // Windows
+#ifdef _WIN32 // Windows
 
-  enum class colors
+enum class colors
+{
+  black = 0,
+  blue = 1,
+  green = 2,
+  cyan = 3,
+  red = 4,
+  magenta = 5,
+  yellow = 6,
+  white = 7,
+  bright_black = 8,
+  bright_blue = 9,
+  bright_green = 10,
+  bright_cyan = 11,
+  bright_red = 12,
+  bright_magenta = 13,
+  bright_yellow = 14,
+  bright_white = 15,
+};
+
+
+  #include <windows.h>
+
+  // The function to print text to the console with
+  // certain color without line break
+  template < typename TypeString >
+  void prcolor(colors color, const TypeString& msg)
   {
-    black = 0,
-    blue = 1,
-    green = 2,
-    cyan = 3,
-    red = 4,
-    magenta = 5,
-    yellow = 6,
-    white = 7,
-    bright_black = 8,
-    bright_blue = 9,
-    bright_green = 10,
-    bright_cyan = 11,
-    bright_red = 12,
-    bright_magenta = 13,
-    bright_yellow = 14,
-    bright_white = 15,
-  };
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(hConsole, static_cast<int>(color));
+    std::cout << msg;
+    SetConsoleTextAttribute(hConsole, static_cast<int>(colors::white));
+  }
+
+  // The function to print text to the console
+  // of a certain color with a line break
+  template < typename TypeString >
+  void prcolorln(colors color, const TypeString& msg)
+  {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SetConsoleTextAttribute(hConsole, static_cast<int>(color));
+    std::cout << msg << std::endl;
+    SetConsoleTextAttribute(hConsole, static_cast<int>(colors::white));
+  }
+
+  void set_color(colors color)
+  {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, static_cast<int>(color));
+  }
+
+  void uncolor()
+  {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, static_cast<int>(colors::white));
+  }
 
 
-    #include <windows.h>
-
-    // The function to print text to the console with
-    // certain color without line break
-    template < typename __T >
-    void color(colors color, const __T& msg)
-    {
-      HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-      SetConsoleTextAttribute(hConsole, static_cast<int>(color));
-      std::cout << msg;
-      SetConsoleTextAttribute(hConsole, 7);
-    }
-
-    // The function to print text to the console
-    // of a certain color with a line break
-    template < typename __T >
-    void colorln(colors color, const __T& msg)
-    {
-      HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-      SetConsoleTextAttribute(hConsole, static_cast<int>(color));
-      std::cout << msg << std::endl;
-      SetConsoleTextAttribute(hConsole, 7);
-    }
-
-  #else // Linux, Mac
+#else // Linux, Mac
 
     enum class colors
     {
@@ -111,16 +124,22 @@ namespace lpct {
 
     // The function to print text to the console with
     // certain color without line break
-    template < typename __T >
-    void color(colors color, const __T& msg) {
+    template < typename TypeString >
+    void prcolor(colors color, const TypeString& msg) {
       std::cout << detail::get_color(color) << msg << "\033[0m";
     }
 
     // The function to print text to the console
     // of a certain color with a line break
-    template < typename __T >
-    void colorln(colors color, const __T& msg) {
+    template < typename TypeString >
+    void prcolorln(colors color, const TypeString& msg) {
       std::cout << detail::get_color(color) << msg << "\033[0m" << std::endl;
+    }
+
+    template < typename TypeString >
+    TypeString get_colorstr(colors color, TypeString&& msg)
+    {
+      return detail::get_color(color) + msg + "\033[0m";
     }
 
   #endif
